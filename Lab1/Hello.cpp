2f3315b67d091d1458104688a9dd5d8b4cabead3
alanochoa1 <alanochoa036@gmail.com>
2025-01-26 00:39:47 -0800

    Hello.cpp
    Visual Studio Hello World Print Code, Lab 1

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,15 @@
+/***
+* {In this lab we will learn about the basics of completing lab and writing reports 
+to help us get accustomed for the rest of this semester}
+**/
+
+#include <iostream>
+#include <cstdlib> 
+using namespace std; 
+
+int main()
+{
+	cout << "Hello World!" << endl; 
+	printf("Hello World again!\n");
+	return 0; 
+}
